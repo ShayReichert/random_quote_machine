d5cda6740448e81b0e0b4adc5ed3214db37e7a83
Shay Reichert <63343303+Kurtzs@users.noreply.github.com>
2020-07-22 12:35:59 +0200

    Updates

---
+++ precache-manifest.d5984897bc07814406a85df43a356793.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44688b5ee40319d69bfe3aac92432c97",
+    "url": "/ShayReichert/github.io/random_quote_machine/index.html"
+  },
+  {
+    "revision": "5237fdf5f97b3c251700",
+    "url": "/ShayReichert/github.io/random_quote_machine/static/css/main.302b4589.chunk.css"
+  },
+  {
+    "revision": "4c46ff639ea39ca1b39f",
+    "url": "/ShayReichert/github.io/random_quote_machine/static/js/2.31b4ac1a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ShayReichert/github.io/random_quote_machine/static/js/2.31b4ac1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5237fdf5f97b3c251700",
+    "url": "/ShayReichert/github.io/random_quote_machine/static/js/main.9df587ef.chunk.js"
+  },
+  {
+    "revision": "d55db6db2987773398db",
+    "url": "/ShayReichert/github.io/random_quote_machine/static/js/runtime-main.da41c72f.js"
+  }
+]);
